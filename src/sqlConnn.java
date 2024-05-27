0dce01ad51498aef67dc276cf64820eef8ecb52a
parsaex7 <parsaexir2005@gmail.com>
2024-05-27 12:38:49 +0330

    test 2

--- src/sqlConnn.java
+++ src/sqlConnn.java
@@ -9,10 +9,12 @@ public class sqlConnn {
         String passWord;
         System.out.println("Enter PassWord: ");
         Scanner in = new Scanner(System.in);
+        System.out.println("Enter userName: ");
+        String userName = in.nextLine();
         passWord = in.nextLine();
         Connection con = DriverManager.getConnection(
                 "jdbc:mysql://127.0.0.1:3306/linked-in"
-                ,"root"
+                ,userName
                 ,passWord
         );
         Statement statement = con.createStatement();
