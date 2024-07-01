2c34f757d1ac83882a5575695bc8216b54406edc
parsaex7 <parsaexir2005@gmail.com>
2024-07-02 03:17:25 +0330

    setup better scene

--- Client/src/main/java/org/example/client/LinkedInApplication.java
+++ Client/src/main/java/org/example/client/LinkedInApplication.java
@@ -10,11 +10,12 @@ import java.io.IOException;
 
 public class LinkedInApplication extends Application {
     public static User user;
+    public static Scene scene;
     @Override
     public void start(Stage stage) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(LinkedInApplication.class.getResource("intro-view.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 950, 670);
+            scene = new Scene(fxmlLoader.load(), 950, 670);
             stage.setTitle("WELCOME");
             stage.setScene(scene);
             stage.setResizable(false);
--- Client/src/main/java/org/example/client/introViewController.java
+++ Client/src/main/java/org/example/client/introViewController.java
@@ -1,5 +1,6 @@
 package org.example.client;
 
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,16 +8,30 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import org.example.model.GlassPane;
 
 public class introViewController {
     @FXML
     private Button loginButton;
     @FXML
     private Button signupButton;
+    @FXML
+    private VBox blurredVbox;
+    @FXML
+    private StackPane mainPane;
+    @FXML
+    private Label welcomeLabel;
+
+    private GlassPane glassPane;
 
     public void loginController(ActionEvent event) {
         try {
+            Functions.buttonAnimation(loginButton);
             FXMLLoader loader = new FXMLLoader(getClass().getResource("login-view.fxml"));
             Parent root = loader.load();
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -29,6 +44,7 @@ public class introViewController {
 
     public void signupController(ActionEvent event) {
         try {
+            Functions.buttonAnimation(signupButton);
             FXMLLoader loader = new FXMLLoader(getClass().getResource("signup-view.fxml"));
             Parent root = loader.load();
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -37,5 +53,23 @@ public class introViewController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    public void initialize() {
+        glassPane = new GlassPane();
+        glassPane.setPickOnBounds(false);
+        mainPane.getChildren().add(glassPane);
+        blurredVbox.setMouseTransparent(false);
+        loginButton.setMouseTransparent(false);
+        signupButton.setMouseTransparent(false);
+        welcomeLabel.setOpacity(0);
+
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(4), welcomeLabel);
+        fadeIn.setFromValue(0);
+        fadeIn.setToValue(1);
+        fadeIn.setCycleCount(1);
+        fadeIn.setAutoReverse(false);
+        fadeIn.play();
     }
 }
